1f18b9272abdee61764a3b0b66b80029ee71242c
SHUKLA123 <32232851+SHUKLA123@users.noreply.github.com>
2020-05-13 20:58:13 +0530

    Sieve Of Eratosthenes.py

---
+++ Sieve Of Eratosthenes.py
@@ -0,0 +1,25 @@
+n = 16
+def SieveOfEratosthenes(n):
+    prime = [True for i in range(n+1)] 
+    p = 2
+    while (p * p <= n): 
+        if (prime[p] == True): 
+            for i in range(p * p, n+1, p): 
+                prime[i] = False
+        p += 1
+    for p in range(2, n+1): 
+        if prime[p]: 
+            print(p)
+
+print(SieveOfEratosthenes(n)) 
+
+
+# Output : 
+#---------
+
+# 2
+# 3
+# 5
+# 7
+# 11
+# 13
